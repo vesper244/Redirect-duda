a9ad38bb63e8bc629f7e12da42a200c488686eb2
vesper244 <venanciocurso2@gmail.com>
2025-09-11 09:55:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 window.onload = function() {
     // Defina o link para o qual você quer redirecionar o usuário
-    const destinationURL = "https://api.whatsapp.com/send/?phone=551119728753&text&type=phone_number&app_absent=0"; // <-- TROQUE ESTE LINK!
+    const destinationURL = "https://api.whatsapp.com/send/?phone=5511919728753&text&type=phone_number&app_absent=0"; // <-- TROQUE ESTE LINK!
 
     // Define o tempo de espera em milissegundos.
     // Para um redirecionamento quase instantâneo, você pode usar um valor baixo como 100 ou até 0.
@@ -13,3 +13,4 @@ window.onload = function() {
     }, delay);
 
 };
+
